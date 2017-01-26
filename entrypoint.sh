f886a021e16ca8882e3f57f6ace9c91264ffb036
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2017-01-26 16:01:18 -0200

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+DAEMON=sshd
+
+if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
+    ssh-keygen -A
+fi
+
+stop() {
+    echo "Received SIGINT or SIGTERM. Shutting down $DAEMON"
+    pid=$(cat /var/run/$DAEMON/$DAEMON.pid)
+    kill -SIGTERM "${pid}"
+    wait "${pid}"
+    echo "Done."
+}
+
+echo "Running $@"
+if [ "$(basename $1)" == "$DAEMON" ]; then
+    trap stop SIGINT SIGTERM
+    $@ &
+    pid="$!"
+    mkdir -p /var/run/$DAEMON && echo "${pid}" > /var/run/$DAEMON/$DAEMON.pid
+    wait "${pid}" && exit $?
+else
+    exec "$@"
+fi
+
